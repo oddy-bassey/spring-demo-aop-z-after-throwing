ac691941cadf48d2ec3ffe8e3196eef4b388fe11
@revoltcode <oddybassey@gmail.com>
2020-11-21 05:28:22 -0500

    first commit

---
+++ src/com/java/aopdemo/AfterThrowingDemoApp.java
@@ -0,0 +1,41 @@
+package com.java.aopdemo;
+
+import java.util.List;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.java.aopdemo.dao.Account;
+import com.java.aopdemo.dao.AccountDAO;
+
+public class AfterThrowingDemoApp {
+	public static void main(String[] args) {
+		
+		//read spring java config class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		//get the AccountDAO bean from spring container
+		AccountDAO accDao = context.getBean("accountDAO", AccountDAO.class);
+		
+		List<Account> accounts = null;
+		try {
+			
+			//add a boolean flag to simulate an exception
+			
+			//call the method to find the Accounts
+			boolean tripWire = true;
+			
+			accounts = accDao.findAccounts(tripWire);
+			
+		}catch (Exception e) {
+			System.out.println("\n\nMain Program ... caught exception: "+e);
+		}
+		
+		//display the Accunts
+		System.out.println("\n\nMain Program: AfterReturningDemoApp");
+		System.out.println("----------");
+		System.out.println(accounts+"\n");
+		
+		//close the
+		context.close();
+	}
+}
